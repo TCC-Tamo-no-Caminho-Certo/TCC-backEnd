8337f338bae4962fb28417c91dd6271b2c16c8bd
João Pedro Caires <joaopdante@gmail.com>
2020-07-25 00:29:23 -0300

    Create run.lua

---
+++ .rgd/run.lua
@@ -0,0 +1,9 @@
+status = run("npm", "install")
+setStatus(status)
+status = run("npm", "run build")
+setStatus(status)
+status = run("rm", "-rfv /home/rgd/deploy/backend/")
+--setStatus(status)
+--status = run("cp", "-rv ./build/ /home/rgd/deploy/frontend/")
+--setStatus(status)
+-- 1.0
