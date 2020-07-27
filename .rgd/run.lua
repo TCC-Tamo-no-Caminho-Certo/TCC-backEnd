23544eb63e15505954694f0084eb89dfabe977c4
João Pedro Caires <joaopdante@gmail.com>
2020-07-27 20:31:53 -0300

    Update run.lua

--- .rgd/run.lua
+++ .rgd/run.lua
@@ -4,4 +4,10 @@ if branch == "development" then
     setStatus(status)
     status = run("npm", "run build")
     setStatus(status)
+elseif branch == "master" then
+    status = run("npm", "install")
+    setStatus(status)
+    status = run("npm", "run build")
+    setStatus(status)
+    status = run("rm", "-rfv /home/rgd/deploy/backend/")
 end
