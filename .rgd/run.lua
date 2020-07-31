a89f15656697ad2a648def39baebb7dc8e791213
João Pedro Caires <joaopdante@gmail.com>
2020-07-31 20:34:10 -0300

    Adiciona PM2 ao script RGD 💚 :rocket:

--- .rgd/run.lua
+++ .rgd/run.lua
@@ -1,4 +1,4 @@
--- Version 1.2.2
+-- Version 1.3.0
 
 function executeWithStatus(filename, args)
     status = run(filename, args)
@@ -16,6 +16,8 @@ function runScript()
         if executeWithStatus("rm", "-rfv /home/rgd/deploy/dev/backend/") then return end
         if executeWithStatus("cp", "-rv ./build/ /home/rgd/deploy/dev/backend/") then return end
         if executeWithStatus("ln", "-s /home/rgd/repositories/55d7eadfa5c14c16a439bdbe547597b0/node_modules /home/rgd/deploy/dev/backend/node_modules") then return end
+        runWD("pm2", "stop backend-dev", "/home/rgd/deploy/dev/backend")
+        runWD("pm2", "start ./bundle.js --name backend-dev -- --config-/home/rgd/deploy/dev/.config/server.json", "/home/rgd/deploy/dev/backend")
     elseif branch == "master" then
         if executeWithStatus("npm", "--no-color install") then return end
         if executeWithStatus("rm", "-rfv ./build/") then return end
@@ -25,6 +27,8 @@ function runScript()
         if executeWithStatus("rm", "-rfv /home/rgd/deploy/dist/backend/") then return end
         if executeWithStatus("cp", "-rv ./build/ /home/rgd/deploy/dist/backend/") then return end
         if executeWithStatus("ln", "-s /home/rgd/repositories/4938f8cdafe94b8682470eacd6d5eae4/node_modules /home/rgd/deploy/dist/backend/node_modules") then return end
+        runWD("pm2", "stop backend-dev", "/home/rgd/deploy/dist/backend")
+        runWD("pm2", "start ./bundle.js --name backend-dev -- --config-/home/rgd/deploy/dist/.config/server.json", "/home/rgd/deploy/dist/backend")
     end
 end
 
