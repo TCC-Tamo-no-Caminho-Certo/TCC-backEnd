ad20c5f8be64c462e503a95a6d6f1bf75dd6d91e
João Pedro Caires <joaopdante@gmail.com>
2020-07-31 20:36:35 -0300

    Atualiza e corrige script RGD 💚 🐛

--- .rgd/run.lua
+++ .rgd/run.lua
@@ -1,4 +1,4 @@
--- Version 1.3.0
+-- Version 1.3.1
 
 function executeWithStatus(filename, args)
     status = run(filename, args)
@@ -27,8 +27,8 @@ function runScript()
         if executeWithStatus("rm", "-rfv /home/rgd/deploy/dist/backend/") then return end
         if executeWithStatus("cp", "-rv ./build/ /home/rgd/deploy/dist/backend/") then return end
         if executeWithStatus("ln", "-s /home/rgd/repositories/4938f8cdafe94b8682470eacd6d5eae4/node_modules /home/rgd/deploy/dist/backend/node_modules") then return end
-        runWD("pm2", "stop backend-dev", "/home/rgd/deploy/dist/backend")
-        runWD("pm2", "start ./bundle.js --name backend-dev -- --config-/home/rgd/deploy/dist/.config/server.json", "/home/rgd/deploy/dist/backend")
+        runWD("pm2", "stop backend-dist", "/home/rgd/deploy/dist/backend")
+        runWD("pm2", "start ./bundle.js --name backend-dist -- --config-/home/rgd/deploy/dist/.config/server.json", "/home/rgd/deploy/dist/backend")
     end
 end
 
