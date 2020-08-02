65b19d79703d6a5198578fbdc5eb95c7fe725e4c
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2020-08-02 20:48:47 -0300

    refatora seed 01 :art:

--- src/database/seeds/01_create_district_city.ts
+++ src/database/seeds/01_create_district_city.ts
@@ -17,17 +17,18 @@ export async function seed(knex: knex) {
         .where({ name: districts[key].nome })
         .then(row => (row[0] ? row[0].id_district : null))
 
-      if (!id_district)
+      if (!id_district) {
         id_district = await trx('district')
           .insert({ name: districts[key].nome, country_id: 1 })
           .then(row => (row[0] ? row[0] : null))
 
-      for (const key in cities) {
-        const has_city = await trx('city')
-          .where({ name: cities[key].nome })
-          .then(row => (row[0] ? row[0].id_city : null))
+        for (const key in cities) {
+          const has_city = await trx('city')
+            .where({ name: cities[key].nome })
+            .then(row => (row[0] ? row[0].id_city : null))
 
-        if (!has_city) insert_city.push({ name: cities[key].nome, district_id: id_district })
+          if (!has_city) insert_city.push({ name: cities[key].nome, district_id: id_district })
+        }
       }
 
       await trx('city').insert(insert_city)
--- src/server.ts
+++ src/server.ts
@@ -1,87 +1,59 @@
-import config, { EndpointConfig } from "./config";
-import version from "./version.generated";
-import controllers from "./controllers";
-import logger from "./services/logger";
-import compression from "compression";
-import redis from "./services/redis";
-import express from "express";
-import https from "https";
-import http from "http";
-import cors from "cors";
-import path from "path";
+import config, { EndpointConfig } from './config'
+import version from './version.generated'
+import controllers from './controllers'
+import logger from './services/logger'
+import compression from 'compression'
+import redis from './services/redis'
+import express from 'express'
+import https from 'https'
+import http from 'http'
+import cors from 'cors'
+import path from 'path'
 
 // Initialize External Modules
 
-logger.initializeLogger(config.logging.path, config.logging.filename);
-logger.info(
-  "Running ArisLabs Backend - " +
-    version.major +
-    "." +
-    version.minor +
-    "." +
-    version.patch +
-    " Build " +
-    version.build
-);
+logger.initializeLogger(config.logging.path, config.logging.filename)
+logger.info('Running ArisLabs Backend - ' + version.major + '.' + version.minor + '.' + version.patch + ' Build ' + version.build)
 
 if (config.redis.use) {
-  logger.info(`Using redis at ${config.redis.host}:${config.redis.port}`);
-  redis.initialize(
-    config.redis.host,
-    config.redis.port,
-    config.redis.database,
-    config.redis.password
-  );
+  logger.info(`Using redis at ${config.redis.host}:${config.redis.port}`)
+  redis.initialize(config.redis.host, config.redis.port, config.redis.database, config.redis.password)
 }
 
 // App Configuration
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-if (config.environment === "production") app.use(compression());
-app.use(
-  "/",
-  express.static(
-    path.resolve(config.server.root.replace("%CurrentDirectory%", __dirname))
-  )
-);
-controllers(app);
+app.use(cors())
+app.use(express.json())
+if (config.environment === 'production') app.use(compression())
+app.use('/', express.static(path.resolve(config.server.root.replace('%CurrentDirectory%', __dirname))))
+controllers(app)
 
 // Version route
 
-app.get("/api/version", (req, res) => {
+app.get('/api/version', (req, res) => {
   res.json({
     major: version.major,
     minor: version.minor,
     patch: version.patch,
     build: version.build,
-    string:
-      version.major +
-      "." +
-      version.minor +
-      "." +
-      version.patch +
-      " Build " +
-      version.build,
-  });
-});
+    string: version.major + '.' + version.minor + '.' + version.patch + ' Build ' + version.build
+  })
+})
 
 // Server Activation
 
 config.server.endpoints.forEach((endpoint: EndpointConfig) => {
   if (endpoint.enabled) {
     if (endpoint.useSSL) {
-      https
-        .createServer(endpoint.options, app)
-        .listen(endpoint.port, endpoint.host);
-      logger?.info(`Listening on https://${endpoint.host}:${endpoint.port}`);
+      https.createServer(endpoint.options, app).listen(endpoint.port, endpoint.host)
+      logger?.info(`Listening on https://${endpoint.host}:${endpoint.port}`)
     } else {
-      http.createServer(app).listen(endpoint.port, endpoint.host);
-      logger?.info(`Listening on http://${endpoint.host}:${endpoint.port}`);
+      http.createServer(app).listen(endpoint.port, endpoint.host)
+      logger?.info(`Listening on http://${endpoint.host}:${endpoint.port}`)
     }
   }
-});
+})
 
-export default app;
+export default app
