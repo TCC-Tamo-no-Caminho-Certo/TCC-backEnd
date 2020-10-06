561cea06449032892a73dfe4bfba71d02fc1f084
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2020-10-06 18:36:04 -0300

    corrige reset de senha :bug:

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -135,8 +135,8 @@ route.post('/api/reset-password', captcha, async (req: Request, res: Response) =
     Data.validate({ password }, 'reset_password')
 
     const user = await User.getUser(id)
-    user.password = await argon.hash(user.password)
-    await user.update({ password })
+    const new_hash = await argon.hash(user.password)
+    await user.update({ password: new_hash })
 
     redis.client.del(`reset.${token}`)
 
