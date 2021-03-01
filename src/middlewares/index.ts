25fb3571a6078ce9c79416a8cced22df7b880f5f
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2021-03-01 18:08:52 -0300

    refatora middleware permission :art:

---
+++ src/middlewares/index.ts
@@ -0,0 +1,5 @@
+import auth from './auth'
+import captcha from './recaptcha'
+import permission from './permission'
+
+export { auth, permission, captcha }
--- src/middlewares/permission.ts
+++ src/middlewares/permission.ts
@@ -1,9 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
-import { RoleTypes } from '../database/models/user/role'
+import { RoleTypes } from '../types'
 
-export default (roles: RoleTypes[], not_allowed: boolean = false) => {
+type NRoleTypes = '!admin' | '!guest' | '!student' | '!professor' | '!customer' | '!evaluator' | '!moderator'
+
+export default (roles: (RoleTypes | NRoleTypes)[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const allow = roles.some(role => req.body._roles.some((user_role: string) => not_allowed ? user_role !== role : user_role === role))
+    const allow = roles.some(role => {
+      const r = role.split('!')
+      const not_allowed = r.length === 1 ? false : true
+      const has_role = req.body._roles.some((user_role: string) => (not_allowed ? user_role === r[1] : user_role === r[0]))
+      return not_allowed ? !has_role : has_role
+    })
     return allow ? next() : res.status(403).send({ success: false, message: `User of type ${req.body._roles} is not allowed to access this route!` })
   }
 }
