a01ad30c64ecbcea067b0c22554cf6a60c6bc608
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2021-03-28 20:07:36 -0300

    Refatora middleware permission :art:

--- src/middlewares/permission.ts
+++ src/middlewares/permission.ts
@@ -5,19 +5,19 @@ type NRoleTypes = '!admin' | '!guest' | '!student' | '!professor' | '!customer' 
 
 export default (roles: (RoleTypes | NRoleTypes)[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const d_roles: string[] = []
 
     const allow = roles.reduce<number>((acc, role) => {
       const r = role.split('!')
-      const flag = r.length === 1 ? false : true
+      const flag = r.length !== 1
 
-      const has_role = req.body._roles.some((user_role: string) => (flag ? user_role === r[1] : user_role === r[0]))
+      const has_role = flag
+        ? req.body._roles.some((user_role: string) => user_role === r[1])
+        : req.body._roles.some((user_role: string) => user_role === r[0])
       const allowed = flag ? !has_role : has_role
 
-      if (!allowed) d_roles.push(flag ? r[1] : r[0])
       return allowed * acc
     }, 1)
-    
-    return allow ? next() : res.status(403).send({ success: false, message: `User of type ${d_roles} is not allowed to access this route!` })
+
+    return allow ? next() : res.status(403).send({ success: false, message: `User not allowed to access this route!`, roles })
   }
 }
