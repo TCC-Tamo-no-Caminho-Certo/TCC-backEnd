28a4c1ca255546311567bdb391fe557cead6f246
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2021-07-17 16:39:57 -0300

    Corrige nome da tabela moderator university :bug:

--- src/database/models/user/moderator_university.ts
+++ src/database/models/user/moderator_university.ts
@@ -80,13 +80,13 @@ export default class Moderator_University1 {
 
 // --------------- //
 
-interface Moderator {
+interface Moderator_University {
   user_id: Foreign
   university_id: Foreign
 }
 
-const Moderator_UniversityModel = new Model<Moderator, never>('moderator', { foreign: ['user_id', 'university_id'] })
+const Moderator_UniversityModel = new Model<Moderator_University, never>('moderator_university', { foreign: ['user_id', 'university_id'] })
 
-type IModerator_UniversityModel = IModel<Moderator, never>
+type IModerator_UniversityModel = IModel<Moderator_University, never>
 
 export { Moderator_UniversityModel, IModerator_UniversityModel }
