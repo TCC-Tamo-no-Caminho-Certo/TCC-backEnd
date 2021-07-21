bf2c0016ab9e7fbd6c20ca036dbb265b826c8b15
Gabriel Nori <53913327+TycoonRaccoon@users.noreply.github.com>
2021-07-21 17:23:48 -0300

    Deleta models antigos :fire:

--- src/database/models/university/course.ts
+++ src/database/models/university/course.ts
@@ -1,44 +1,5 @@
-import ArisError from '../../../utils/arisError'
 import { CourseTypes } from '../../../@types/types'
 import { Model, Increment, IModel } from '..'
-import db from '../..'
-
-let courses: Required<CourseCtor>[] = []
-
-db('course').then(row => {
-  if (!row) throw new ArisError('Couldn´t get all courses', 500)
-  courses.push(...row)
-})
-
-export interface CourseCtor {
-  course_id?: number
-  name: CourseTypes
-}
-
-export default class Course1 {
-  protected course_id: number
-  protected name: CourseTypes
-
-  /**
-   * Creates a course.
-   */
-  protected constructor({ course_id, name }: CourseCtor) {
-    this.course_id = course_id || 0 //Gives a temporary id when creating a new course
-    this.name = name
-  }
-
-  protected static _find(identifier: CourseTypes | number) {
-    const course = courses.find(course => (typeof identifier === 'string' ? course.name === identifier : course.course_id === identifier))
-    if (!course) throw new ArisError(`Course provided does't exists!`, 400)
-    return course
-  }
-
-  protected static _findAll() {
-    return courses
-  }
-}
-
-// --------------- //
 
 interface Course {
   id: Increment
--- src/database/models/user/role.ts
+++ src/database/models/user/role.ts
@@ -1,45 +1,5 @@
-import ArisError from '../../../utils/arisError'
-import { Model, Foreign, IModel } from '..'
-import db from '../..'
-
 import { RoleTypes } from '../../../@types/types'
-
-const roles: Required<RoleCtor>[] = []
-
-// db<Required<RoleCtor>>('role').then(row => {
-//   if (!row[0]) throw new ArisError('Couldn´t get all roles', 500)
-//   roles.push(...row)
-// })
-
-export interface RoleCtor<T extends RoleTypes = RoleTypes> {
-  role_id?: number
-  title: T
-}
-
-export default class Role1<T extends RoleTypes = RoleTypes> {
-  protected role_id: number
-  protected title: T
-
-  /**
-   * Creates a role.
-   */
-  protected constructor({ role_id, title }: RoleCtor<T>) {
-    this.role_id = role_id || 0 //Gives a temporary id when creating a new role
-    this.title = title
-  }
-
-  protected static _find<T extends RoleTypes | number>(identifier: T) {
-    const role = roles.find(role => (typeof identifier === 'string' ? role.title === identifier : role.role_id === identifier))
-    if (!role) throw new ArisError(`Role provided does't exists!`, 400)
-    return <T extends RoleTypes ? Required<RoleCtor<T>> : Required<RoleCtor>>role
-  }
-
-  protected static _findAll() {
-    return roles
-  }
-}
-
-// --------------- //
+import { Model, Foreign, IModel } from '..'
 
 type RolesList = { [Key in RoleTypes]: boolean }
 
@@ -47,7 +7,7 @@ interface Role extends RolesList {
   user_id: Foreign
 }
 
-type RoleUp = Partial<Omit<Role, "user_id">>
+type RoleUp = Partial<Omit<Role, 'user_id'>>
 type RoleIn = Pick<Role, 'user_id'>
 
 const RoleModel = new Model<Role, RoleUp, RoleIn>('role', { foreign: ['user_id'] })
